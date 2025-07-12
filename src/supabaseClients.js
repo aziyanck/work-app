4f3f19ecbb032c853130e03ee8ef11cd2240e89c
aziyanck <aziyan916@gmail.com>
2025-07-12 20:20:00 +0530

    all error fix

--- src/supabaseClients.js
+++ src/supabaseClients.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://YOUR_PROJECT.supabase.co'
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY'
+const supabaseUrl = 'https://rnhovnbsdiufqdectyjz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuaG92bmJzZGl1ZnFkZWN0eWp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNjY4MzgsImV4cCI6MjA2Nzg0MjgzOH0.XEWDAXTSyECdJLm1q6vRLsWQ4LBAzSy__MaP7_PbHfM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
